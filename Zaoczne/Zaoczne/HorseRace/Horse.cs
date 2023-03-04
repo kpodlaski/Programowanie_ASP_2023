e940df32b257ba485d7186622d574e51086606d9
kpodlaski <podlaski@uni.lodz.pl>
2023-03-04 14:31:42 +0100

    Horse race

---
+++ Zaoczne/Zaoczne/HorseRace/Horse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zaoczne.HorseRace
+{
+    class Horse
+    {
+        public int ID { get; private set; }
+        private double speed;
+        static int lastId = 0;
+
+        public Horse(double speed)
+        {
+            ID = lastId++;
+            this.speed = speed;
+        }
+
+        public void DoRace(Race race)
+        {
+            Console.WriteLine(String.Format(
+                "Approaching start line {0}", ID));
+            double distance = 0.0;
+            while (distance < race.Distance)
+            {
+                distance += speed;
+            }
+            race.HorseFinished(this);
+            Console.WriteLine(String.Format("Horse {0} ended Race", ID));
+
+        }
+
+
+    }
+}
---
+++ Zaoczne/Zaoczne/HorseRace/Race.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Zaoczne.HorseRace
+{
+    class Race
+    {
+        public double Distance = 1000;
+        private List<Horse> horses = new List<Horse>();
+
+        public void SetUpRace()
+        {
+            
+            Random rand = new Random();
+            for (int i=0; i<5; i++)
+            {
+                Horse h = new Horse(rand.NextDouble()*50+1);
+                horses.Add(h);
+            }
+
+            foreach(Horse h in horses)
+            {
+                Thread t = new Thread(new ThreadStart(() =>
+                {
+                    h.DoRace(this);
+                }));
+                t.Start();
+            }
+        }
+
+        public void HorseFinished(Horse horse)
+        {
+            Console.WriteLine(String.Format("Horse {0} finished Race", horse.ID));
+        }
+
+    }
+}
